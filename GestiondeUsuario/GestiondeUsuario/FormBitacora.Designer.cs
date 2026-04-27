b4ffff5c9f51cf58f8c9dd4febfbcbe1a686ebce
camilitas <camilaaguilera2004@gmail.com>
2026-04-27 20:22:01 -0300

    agrego FormBitacora con filtros por fecha, login, modulo, evento y criticidad

--- GestiondeUsuario/GestiondeUsuario/FormBitacora.Designer.cs
+++ GestiondeUsuario/GestiondeUsuario/FormBitacora.Designer.cs
@@ -28,12 +28,284 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvBitacora = new System.Windows.Forms.DataGridView();
+            this.lblBitacora = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.cmbLogin = new System.Windows.Forms.ComboBox();
+            this.cmbModulo = new System.Windows.Forms.ComboBox();
+            this.dtpFechaIni = new System.Windows.Forms.DateTimePicker();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblModulo = new System.Windows.Forms.Label();
+            this.lblEvento = new System.Windows.Forms.Label();
+            this.cmbEvento = new System.Windows.Forms.ComboBox();
+            this.lblCriticidad = new System.Windows.Forms.Label();
+            this.cmbCriticidad = new System.Windows.Forms.ComboBox();
+            this.lblFechaIni = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpFechaFin = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBitacora)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBitacora
+            // 
+            this.dgvBitacora.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBitacora.Location = new System.Drawing.Point(104, 52);
+            this.dgvBitacora.Name = "dgvBitacora";
+            this.dgvBitacora.ReadOnly = true;
+            this.dgvBitacora.Size = new System.Drawing.Size(587, 232);
+            this.dgvBitacora.TabIndex = 0;
+            this.dgvBitacora.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // lblBitacora
+            // 
+            this.lblBitacora.AutoSize = true;
+            this.lblBitacora.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBitacora.Location = new System.Drawing.Point(33, 20);
+            this.lblBitacora.Name = "lblBitacora";
+            this.lblBitacora.Size = new System.Drawing.Size(177, 24);
+            this.lblBitacora.TabIndex = 1;
+            this.lblBitacora.Text = "Bitacora de Eventos";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(687, 12);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(151, 307);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ReadOnly = true;
+            this.txtNombre.Size = new System.Drawing.Size(202, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(489, 303);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.ReadOnly = true;
+            this.txtApellido.Size = new System.Drawing.Size(202, 20);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // cmbLogin
+            // 
+            this.cmbLogin.FormattingEnabled = true;
+            this.cmbLogin.Location = new System.Drawing.Point(88, 341);
+            this.cmbLogin.Name = "cmbLogin";
+            this.cmbLogin.Size = new System.Drawing.Size(143, 21);
+            this.cmbLogin.TabIndex = 5;
+            // 
+            // cmbModulo
+            // 
+            this.cmbModulo.FormattingEnabled = true;
+            this.cmbModulo.Location = new System.Drawing.Point(104, 384);
+            this.cmbModulo.Name = "cmbModulo";
+            this.cmbModulo.Size = new System.Drawing.Size(143, 21);
+            this.cmbModulo.TabIndex = 8;
+            // 
+            // dtpFechaIni
+            // 
+            this.dtpFechaIni.Location = new System.Drawing.Point(302, 344);
+            this.dtpFechaIni.Name = "dtpFechaIni";
+            this.dtpFechaIni.ShowCheckBox = true;
+            this.dtpFechaIni.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaIni.TabIndex = 9;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(440, 447);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 11;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(359, 447);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 12;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Location = new System.Drawing.Point(278, 447);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnAplicar.TabIndex = 13;
+            this.btnAplicar.Text = "Aplicar";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            this.btnAplicar.Click += new System.EventHandler(this.btnAplicar_Click);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(437, 310);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 14;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(101, 310);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 15;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(38, 344);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(33, 13);
+            this.lblLogin.TabIndex = 16;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblModulo
+            // 
+            this.lblModulo.AutoSize = true;
+            this.lblModulo.Location = new System.Drawing.Point(54, 387);
+            this.lblModulo.Name = "lblModulo";
+            this.lblModulo.Size = new System.Drawing.Size(42, 13);
+            this.lblModulo.TabIndex = 17;
+            this.lblModulo.Text = "Modulo";
+            // 
+            // lblEvento
+            // 
+            this.lblEvento.AutoSize = true;
+            this.lblEvento.Location = new System.Drawing.Point(288, 390);
+            this.lblEvento.Name = "lblEvento";
+            this.lblEvento.Size = new System.Drawing.Size(41, 13);
+            this.lblEvento.TabIndex = 19;
+            this.lblEvento.Text = "Evento";
+            // 
+            // cmbEvento
+            // 
+            this.cmbEvento.FormattingEnabled = true;
+            this.cmbEvento.Location = new System.Drawing.Point(338, 384);
+            this.cmbEvento.Name = "cmbEvento";
+            this.cmbEvento.Size = new System.Drawing.Size(143, 21);
+            this.cmbEvento.TabIndex = 18;
+            // 
+            // lblCriticidad
+            // 
+            this.lblCriticidad.AutoSize = true;
+            this.lblCriticidad.Location = new System.Drawing.Point(492, 387);
+            this.lblCriticidad.Name = "lblCriticidad";
+            this.lblCriticidad.Size = new System.Drawing.Size(50, 13);
+            this.lblCriticidad.TabIndex = 21;
+            this.lblCriticidad.Text = "Criticidad";
+            // 
+            // cmbCriticidad
+            // 
+            this.cmbCriticidad.FormattingEnabled = true;
+            this.cmbCriticidad.Location = new System.Drawing.Point(548, 384);
+            this.cmbCriticidad.Name = "cmbCriticidad";
+            this.cmbCriticidad.Size = new System.Drawing.Size(143, 21);
+            this.cmbCriticidad.TabIndex = 20;
+            // 
+            // lblFechaIni
+            // 
+            this.lblFechaIni.AutoSize = true;
+            this.lblFechaIni.Location = new System.Drawing.Point(245, 346);
+            this.lblFechaIni.Name = "lblFechaIni";
+            this.lblFechaIni.Size = new System.Drawing.Size(51, 13);
+            this.lblFechaIni.TabIndex = 22;
+            this.lblFechaIni.Text = "Fecha Ini";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 346);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Fecha Ini";
+            // 
+            // dtpFechaFin
+            // 
+            this.dtpFechaFin.Location = new System.Drawing.Point(572, 344);
+            this.dtpFechaFin.Name = "dtpFechaFin";
+            this.dtpFechaFin.ShowCheckBox = true;
+            this.dtpFechaFin.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaFin.TabIndex = 23;
+            // 
+            // FormBitacora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 492);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpFechaFin);
+            this.Controls.Add(this.lblFechaIni);
+            this.Controls.Add(this.lblCriticidad);
+            this.Controls.Add(this.cmbCriticidad);
+            this.Controls.Add(this.lblEvento);
+            this.Controls.Add(this.cmbEvento);
+            this.Controls.Add(this.lblModulo);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.dtpFechaIni);
+            this.Controls.Add(this.cmbModulo);
+            this.Controls.Add(this.cmbLogin);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblBitacora);
+            this.Controls.Add(this.dgvBitacora);
+            this.Name = "FormBitacora";
             this.Text = "FormBitacora";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBitacora)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvBitacora;
+        private System.Windows.Forms.Label lblBitacora;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.ComboBox cmbLogin;
+        private System.Windows.Forms.ComboBox cmbModulo;
+        private System.Windows.Forms.DateTimePicker dtpFechaIni;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblModulo;
+        private System.Windows.Forms.Label lblEvento;
+        private System.Windows.Forms.ComboBox cmbEvento;
+        private System.Windows.Forms.Label lblCriticidad;
+        private System.Windows.Forms.ComboBox cmbCriticidad;
+        private System.Windows.Forms.Label lblFechaIni;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFechaFin;
     }
 }
--- GestiondeUsuario/GestiondeUsuario/FormBitacora.cs
+++ GestiondeUsuario/GestiondeUsuario/FormBitacora.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DAL;
 
 namespace GestiondeUsuario
 {
@@ -15,6 +16,137 @@ namespace GestiondeUsuario
         public FormBitacora()
         {
             InitializeComponent();
+        }
+
+        private void FormBitacora_Load(object sender, EventArgs e)
+        {
+            CargarCombos();
+            CargarGrilla();
+        }
+
+        private void CargarCombos()
+        {
+            BitacoraDAL dal = new BitacoraDAL();
+
+            cmbLogin.Items.Clear();
+            cmbLogin.Items.Add("");
+            foreach (var l in dal.ObtenerLogins())
+                cmbLogin.Items.Add(l);
+            cmbLogin.SelectedIndex = 0;
+
+            cmbModulo.Items.Clear();
+            cmbModulo.Items.Add("");
+            cmbModulo.Items.Add("Usuarios");
+            cmbModulo.Items.Add("Ventas");
+            cmbModulo.Items.Add("Compras");
+            cmbModulo.Items.Add("Maestro");
+            cmbModulo.SelectedIndex = 0;
+
+            cmbEvento.Items.Clear();
+            cmbEvento.Items.Add("");
+            cmbEvento.Items.Add("Login");
+            cmbEvento.Items.Add("Logout");
+            cmbEvento.Items.Add("Cambiar Clave");
+            cmbEvento.Items.Add("Crear Usuario");
+            cmbEvento.Items.Add("Bloquear Usuario");
+            cmbEvento.Items.Add("Login fallido");
+            cmbEvento.SelectedIndex = 0;
+
+            cmbCriticidad.Items.Clear();
+            cmbCriticidad.Items.Add("");
+            for (int i = 1; i <= 5; i++)
+                cmbCriticidad.Items.Add(i.ToString());
+            cmbCriticidad.SelectedIndex = 0;
+        }
+
+        private void CargarGrilla()
+        {
+            BitacoraDAL dal = new BitacoraDAL();
+
+            string login = cmbLogin.SelectedItem?.ToString();
+            DateTime? fechaIni = dtpFechaIni.Checked ? dtpFechaIni.Value.Date : (DateTime?)null;
+            DateTime? fechaFin = dtpFechaFin.Checked ? dtpFechaFin.Value.Date : (DateTime?)null;
+            string modulo = cmbModulo.SelectedItem?.ToString();
+            string evento = cmbEvento.SelectedItem?.ToString();
+            int? criticidad = null;
+            if (cmbCriticidad.SelectedIndex > 0)
+                criticidad = int.Parse(cmbCriticidad.SelectedItem.ToString());
+
+            var lista = dal.ObtenerFiltrado(
+                login, fechaIni, fechaFin, modulo, evento, criticidad);
+
+            // Proyectamos para separar Fecha y Hora en columnas distintas
+            var vista = new List<object>();
+            foreach (var b in lista)
+            {
+                vista.Add(new
+                {
+                    Login = b.Usuario,
+                    Fecha = b.Fecha.ToShortDateString(),
+                    Hora = b.Fecha.ToString("HH:mm"),
+                    Modulo = b.Modulo,
+                    Evento = b.Accion,
+                    Criticidad = b.Criticidad,
+                    _nombre = b.Nombre,
+                    _apellido = b.Apellido
+                });
+            }
+
+            dgvBitacora.DataSource = null;
+            dgvBitacora.DataSource = vista;
+
+            if (dgvBitacora.Columns.Contains("_nombre"))
+                dgvBitacora.Columns["_nombre"].Visible = false;
+            if (dgvBitacora.Columns.Contains("_apellido"))
+                dgvBitacora.Columns["_apellido"].Visible = false;
+
+            // Seleccionar primer registro por defecto
+            if (dgvBitacora.Rows.Count > 0)
+            {
+                dgvBitacora.Rows[0].Selected = true;
+                MostrarNombreApellido();
+            }
+            else
+            {
+                txtNombre.Text = "";
+                txtApellido.Text = "";
+            }
+        }
+
+        private void MostrarNombreApellido()
+        {
+            if (dgvBitacora.SelectedRows.Count == 0) return;
+            var fila = dgvBitacora.SelectedRows[0];
+            txtNombre.Text = fila.Cells["_nombre"].Value?.ToString() ?? "";
+            txtApellido.Text = fila.Cells["_apellido"].Value?.ToString() ?? "";
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            MostrarNombreApellido();
+        }
+
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
+            cmbLogin.SelectedIndex = 0;
+            cmbModulo.SelectedIndex = 0;
+            cmbEvento.SelectedIndex = 0;
+            cmbCriticidad.SelectedIndex = 0;
+            dtpFechaIni.Checked = false;
+            dtpFechaFin.Checked = false;
+            CargarGrilla();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            new FormPrincipal().Show();
+            this.Close();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Función de impresión no implementada aún.",
+               "Imprimir", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
