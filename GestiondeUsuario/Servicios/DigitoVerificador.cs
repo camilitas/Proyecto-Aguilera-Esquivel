2e6f4deb604c7c85296052622ef6f295dbb94838
camilitas <camilaaguilera2004@gmail.com>
2026-07-07 19:34:06 -0300

    Servicios: clase DigitoVerificador

---
+++ GestiondeUsuario/Servicios/DigitoVerificador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class DigitoVerificador
+    {
+        public string Tabla { get; set; }
+        public string DVH { get; set; }
+        public string DVV { get; set; }
+    }
+}
