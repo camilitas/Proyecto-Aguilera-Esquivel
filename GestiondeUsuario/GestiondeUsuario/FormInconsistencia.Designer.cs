535f2ceabff7ea8c3e73927266b09c34740d6b9e
camilitas <camilaaguilera2004@gmail.com>
2026-07-07 19:46:06 -0300

    UI: FormInconsistencia

---
+++ GestiondeUsuario/GestiondeUsuario/FormInconsistencia.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GestiondeUsuario
+{
+    partial class FormInconsistencia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnRecalcular = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(66, 56);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "label1";
+            // 
+            // btnRecalcular
+            // 
+            this.btnRecalcular.Location = new System.Drawing.Point(69, 116);
+            this.btnRecalcular.Name = "btnRecalcular";
+            this.btnRecalcular.Size = new System.Drawing.Size(75, 40);
+            this.btnRecalcular.TabIndex = 1;
+            this.btnRecalcular.Text = "Recalcular";
+            this.btnRecalcular.UseVisualStyleBackColor = true;
+            this.btnRecalcular.Click += new System.EventHandler(this.btnRecalcular_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(194, 116);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(75, 40);
+            this.btnRestore.TabIndex = 2;
+            this.btnRestore.Text = "Ir a Restaurar";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(321, 116);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 40);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FormInconsistencia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 188);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnRecalcular);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "FormInconsistencia";
+            this.Text = "FormInconsistencia";
+            this.Load += new System.EventHandler(this.FormInconsistencia_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnRecalcular;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ GestiondeUsuario/GestiondeUsuario/FormInconsistencia.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+
+namespace GestiondeUsuario
+{
+    public partial class FormInconsistencia : Form
+    {
+        public FormInconsistencia()
+        {
+            InitializeComponent();
+        }
+
+        private void FormInconsistencia_Load(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "Se detectó una inconsistencia en los datos del sistema.\n" +
+                             "Por favor seleccioná una opción para continuar.";
+        }
+
+        private void btnRecalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DigitoVerificadorBLL.Instancia.RecalcularYGuardar();
+                MessageBox.Show("Dígito verificador recalculado correctamente.\n" +
+                                "El sistema aceptará los datos actuales como válidos.",
+                    "Recalculado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                new Form1().Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al recalcular: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            new FormRestore().Show();
+            this.Close();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
